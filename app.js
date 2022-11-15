ef08bd81dabbd34ef299484c52b8f9bbc089dde5
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-11-15 05:13:12 -0600

    started styling desktop view

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const burgerButton = document.getElementById("the-burger");
-const navList = document.getElementById("nav-list")
+const navList = document.getElementById("nav-list");
+const x = document.getElementById("x");
 
 burgerButton.addEventListener("click", navPopup);
+x.addEventListener("click", navClose);
 
 function navPopup() {
     navList.style.display = "block";
+    x.style.display = "block";
+    burgerButton.style.display = "none";
+}
+
+function navClose() {
+    navList.style.display = "none";
+    x.style.display = "none";
+    burgerButton.style.display = "block";
 }
